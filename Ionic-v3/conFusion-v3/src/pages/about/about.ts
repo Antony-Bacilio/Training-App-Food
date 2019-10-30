1e19f08bf560c7262795372f851c2be58c701c98
Antony-Bacilio <antonybaciliosalazar@gmail.com>
2019-10-30 23:08:55 +0100

    Ajout contenu au menu About par AboutProvider-service

--- Ionic-v3/conFusion-v3/src/pages/about/about.ts
+++ Ionic-v3/conFusion-v3/src/pages/about/about.ts
@@ -1,5 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core'; /** Ajout interface 'OnInit' et 'Inject' */
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/** Appel  à l'interface 'Leader' avec ses variables*/
+import { Leader } from '../../shared/leader'; 
+
+/** Appel au Provider (Service) permettant la 'comunication' (demande/récuperation données) avec la BdD (db.json)  */
+import { LeaderProvider } from '../../providers/leader/leader'; 
+
+
+
 
 /**
  * Generated class for the AboutPage page.
@@ -13,9 +22,22 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
   selector: 'page-about',
   templateUrl: 'about.html',
 })
-export class AboutPage {
+export class AboutPage implements OnInit{
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  leaders: Leader[];
+  leaderErrMess: string;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams,
+    private leaderService: LeaderProvider,
+    @Inject('BaseURL') private BaseURL) {
+  }
+
+  ngOnInit(){
+    /** Appel fonction 'getLeaders' et pas 'getFeatureLeader()' ou 'getLeader()' de LeaderProvider.ts */
+    this.leaderService.getLeaders() 
+    .subscribe(leaders => this.leaders = leaders, 
+        errmess => this.leaderErrMess = <any>errmess);
+
   }
 
   ionViewDidLoad() {
